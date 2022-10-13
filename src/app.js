661ae9978dd2005862f5f88a2a978707c3674d7e
Sathish J <102170782+sathishcs20@users.noreply.github.com>
2022-10-13 20:58:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 const express =require('express')
+const express =require('express')
+const app= express()
+const mongodb=require('mongodb')
+const MongoClient = mongodb.MongoClient
+const url = 'mongodb://localhost:27017'
+
+
+app.use(express.json())
+app.use(express.static('C:/Users/jssat/OneDrive/Desktop/Project/public'))
+app.set('view engine','hbs')
+app.set('views','C:/Users/jssat/OneDrive/Desktop/Project/views')
+
