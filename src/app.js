29168a95e52dcd68c6aadaf5f8969fee1d27c4e7
Sathish J <102170782+sathishcs20@users.noreply.github.com>
2022-10-14 21:23:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,4 +10,27 @@ app.use(express.json())
 app.use(express.static('C:/Users/jssat/OneDrive/Desktop/Project/public'))
 app.set('view engine','hbs')
 app.set('views','C:/Users/jssat/OneDrive/Desktop/Project/views')
+app.get('/',(req,res)=>
+{
+    res.render('index')
+})
 
+app.post('/search',(req,res)=>
+{
+    console.log(req.body)
+    MongoClient.connect(url,{useNewUrlParser :true},(error,response)=>
+{
+    if(error)
+    {
+        return console.log('unable to connect to databse')
+    }
+
+    const db=response.db('Project')
+
+    db.collection(req.body.lab).find({role:req.body.role}).toArray((e,r)=>
+    {
+        res.send(r)
+    })
+
+})
+})
