221406990e767efb8a13e982f90bb352c080768b
Sathish J <102170782+sathishcs20@users.noreply.github.com>
2022-10-27 00:02:54 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+const result = document.querySelector('.con');
+const box=document.querySelector('.res')
+function speak(sentence) 
+{
+  
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+    
+    
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
 
+    window.speechSynthesis.speak(text_speak);
+}
