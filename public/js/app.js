93d60ad79abbe22bc4501546291c4dd3a2994b45
Sathish J <102170782+sathishcs20@users.noreply.github.com>
2022-10-27 00:04:11 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,37 @@ function speak(sentence)
 
     window.speechSynthesis.speak(text_speak);
 }
+function wishMe() 
+  { 
+    
+    const speech = new SpeechSynthesisUtterance();
+    
+   
+    console.log("hi")
+    var day = new Date();
+    var hr = day.getHours();
+
+    if(hr >= 0 && hr < 12) {
+        speech.text="good morning";
+    }
+
+    else if(hr == 12) {
+        speech.text="good noon";
+    }
+
+    else if(hr > 12 && hr <= 17) {
+        speech.text="good after noon";
+    }
+
+    else {
+       speech.text="good evening";
+    }
+    
+          
+          speech.volume = 1;
+          speech.pitch = 1;
+          speech.rate = 1;
+
+          window.speechSynthesis.speak(speech)
+
+}
