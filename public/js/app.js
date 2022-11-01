0794ce5ad553fc670bddda456bc838f745377e89
Sathish J <102170782+sathishcs20@users.noreply.github.com>
2022-11-01 22:18:59 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -97,3 +97,8 @@ else if(message.includes('what is') || message.includes('what are')) {
         const finalText = "This is what i found on internet regarding " + message;
         speech.text = finalText;
     }
+ else if(message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+        const finalText = "This is what i found on wikipedia regarding " + message;
+        speech.text = finalText;
+    }
