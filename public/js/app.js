969d560f1e74e1288cd2e8bb6399efe97849d562
Sathish J <102170782+sathishcs20@users.noreply.github.com>
2022-11-12 13:58:33 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 const result = document.querySelector('.con');
-const box=document.querySelector('.res')
+
 function speak(sentence) 
 {
   
